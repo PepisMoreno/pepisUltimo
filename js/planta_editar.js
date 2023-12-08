e2447d700e5a407be72a04607371beaffa5f2aff
PepisMoreno <josefinarmoreno@gmail.com>
2023-12-08 09:06:27 -0300

    fijarse qué va de planta_editar.js adentro

--- js/planta_editar.js
+++ js/planta_editar.js
@@ -1,5 +1,5 @@
 console.log(location.search) // lee los argumentos pasados a este formulario
-var id=location.search.substr(4)
+var id=location.search.substring(4)
 console.log(id)
 const { createApp } = Vue
 createApp({
@@ -56,3 +56,61 @@ created() {
 this.fetchData(this.url)
 },
 }).mount('#app')
+
+
+//FIN DE UNA VERSION DE EDITAR.JS
+
+
+
+
+
+
+console.log(location.search) // lee los argumentos pasados a este formulario
+var args=location.search.substr(1).split('&');
+//separa el string por los “&” creando una lista [“id=3” , “nombreComun=’papa’” , ”nombreCientifico=solanaceae”,”imagen=null”]
+console.log(args)
+var parts = []
+for (let i = 0; i < args.length; ++i) {
+    parts[i] = args[i].split('=');
+}
+//decodeUriComponent elimina los caracteres especiales que recibe en la URL 
+document.getElementById("id").value = decodeURIComponent(parts[0][1])
+document.getElementById("nombreComun").value = decodeURIComponent(parts[1][1])
+document.getElementById("nombreCientifico").value = decodeURIComponent(parts[2][1])
+document.getElementById("imagen").value =decodeURIComponent( parts[3][1])
+
+function modificar() {
+    let id = document.getElementById("id").value
+    let n = document.getElementById("nombreComun").value
+    let c = parseFloat(document.getElementById("nombreCientifico").value)
+    let i = parseInt(document.getElementById("imagen").value)
+       
+    let planta = {
+        nombreComun: n,
+        nombreCientifico: c,
+        imagen: i,
+        
+    }
+    let url = "http://localhost:5000/plantas/"+id
+    var options = {
+        body: JSON.stringify(planta),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("modificado")
+            alert("Registro modificado")
+            window.location.href = "./plantas.html";  
+        //NUEVO,  si les da error el fetch  comentar esta linea que puede dar error  
+        })
+        .catch(err => {
+            //this.errored = true
+            console.error(err);
+            alert("Error al Modificar")
+        })      
+}
+
+
+
